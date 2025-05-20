5e815e76da342302e938add703810e80117748ce
Faraytodi Atmajaya <179775310+maybeatmajaya@users.noreply.github.com>
2025-05-20 14:32:49 +0700

    update API token

---
+++ build/app/intermediates/cxx/Debug/1t626i2m/logs/x86/configure_command.bat
@@ -0,0 +1,20 @@
+@echo off
+"D:\\Developer\\androidSDK\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HD:\\Developer\\flutter\\packages\\flutter_tools\\gradle\\src\\main\\groovy" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=x86" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
+  "-DANDROID_NDK=D:\\Developer\\androidSDK\\ndk\\26.3.11579264" ^
+  "-DCMAKE_ANDROID_NDK=D:\\Developer\\androidSDK\\ndk\\26.3.11579264" ^
+  "-DCMAKE_TOOLCHAIN_FILE=D:\\Developer\\androidSDK\\ndk\\26.3.11579264\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=D:\\Developer\\androidSDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=D:\\Project\\tracking_api\\build\\app\\intermediates\\cxx\\Debug\\1t626i2m\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=D:\\Project\\tracking_api\\build\\app\\intermediates\\cxx\\Debug\\1t626i2m\\obj\\x86" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BD:\\Project\\tracking_api\\android\\app\\.cxx\\Debug\\1t626i2m\\x86" ^
+  -GNinja ^
+  -Wno-dev ^
+  --no-warn-unused-cli
